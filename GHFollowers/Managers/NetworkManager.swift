85cd7f1afba015014e4ea824397e5b4447ef4c9b
GeorgeCremer <george.cremer@gmail.com>
2021-03-25 14:00:29 +0000

    Comments removed

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -15,45 +15,6 @@ class NetworkManager {
     
     private init() {}
     
-    // OLD WAY - Without Result Type
-    
-//    func getFollowers(for Username: String, page: Int, completed: @escaping([Follower]?, ErrorMessage?) -> Void) {
-//
-//        let endpoint = baseURL + "\(Username)/followers?per_page=100&page=\(page)"
-//
-//        guard let url = URL(string: endpoint) else {
-//            completed(nil, .invalidUserName)
-//            return
-//        }
-//
-//        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-//            if let _ = error {
-//                completed(nil, .unableToComplete)
-//            }
-//
-//            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-//                completed(nil, .invalidResponse)
-//                return
-//            }
-//
-//            guard let data = data else {
-//                completed(nil, .invalidData)
-//                return
-//
-//            }
-//
-//
-//            do {
-//                let decoder = JSONDecoder()
-//                decoder.keyDecodingStrategy = .convertFromSnakeCase
-//                let followers = try decoder.decode([Follower].self, from: data)
-//                completed(followers,nil)
-//            } catch {
-//                completed(nil, .invalidData)
-//            }
-//        }
-    
-    
     func getFollowers(for Username: String, page: Int, completed: @escaping(Result<[Follower], GFError>) -> Void) {
      
         let endpoint = baseURL + "\(Username)/followers?per_page=100&page=\(page)"
--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -8,15 +8,6 @@
 import Foundation
 
 
-
-//enum ErrorMessage: String {
-//    case invalidUserName = "This username created an invalid requst please try again."
-//    case unableToComplete = "Unable to complete your request please check your internet connection"
-//    case invalidResponse = "Invalid response from the server. Please try again."
-//    case invalidData = "The data recieved form the server was invalid. Please try again."
-//}
-
-
 enum GFError: String, Error {
     case invalidUserName = "This username created an invalid requst please try again."
     case unableToComplete = "Unable to complete your request please check your internet connection"
