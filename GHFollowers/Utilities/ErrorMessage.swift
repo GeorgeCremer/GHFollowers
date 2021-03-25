35355035263091aa60230fc7f5817c6555173346
GeorgeCremer <george.cremer@gmail.com>
2021-03-25 12:10:58 +0000

    Completed error message refactoring

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,18 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by George Cremer on 25/03/2021.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUserName = "This username created an invalid requst please try again."
+    case unableToComplete = "Unable to complete your request please check your internet connection"
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data recieved form the server was invalid. Please try again."
+}
+
+
+
