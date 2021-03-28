993419e6fad9a1e9848a5043dbc47be0288ffb2d
GeorgeCremer <george.cremer@gmail.com>
2021-03-28 12:35:48 +0100

    Persistence Manager Created

--- GHFollowers/Utilities/ErrorMessage.swift
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -13,4 +13,7 @@ enum GFError: String, Error {
     case unableToComplete = "Unable to complete your request please check your internet connection"
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data recieved form the server was invalid. Please try again."
+    case unableToFavourite = "There was an error favouriting this user please try again."
+    case alreadyInFavourites = "You've already favourited this user, you must REALLY like them!"
+
 }
