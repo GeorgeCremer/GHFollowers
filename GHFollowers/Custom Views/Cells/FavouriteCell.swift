c7cf4ef09de53809a8580abaec42447d02e36376
GeorgeCremer <george.cremer@gmail.com>
2021-03-29 12:00:57 +0100

    Updated constraints of Favourites Cell to be in constants.

--- GHFollowers/Custom Views/Cells/FavouriteCell.swift
+++ GHFollowers/Custom Views/Cells/FavouriteCell.swift
@@ -35,17 +35,20 @@ class FavouriteCell: UITableViewCell {
         addSubview(usernameLabel)
         accessoryType = .disclosureIndicator
         let padding: CGFloat = 12
+        let avatarHeight: CGFloat = 60
+        let userNameHeight: CGFloat = 40
+        
         
         NSLayoutConstraint.activate([
             avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
-            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: avatarHeight),
+            avatarImageView.widthAnchor.constraint(equalToConstant: avatarHeight),
             
             usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
             usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
-            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+            usernameLabel.heightAnchor.constraint(equalToConstant: userNameHeight)
         ])
         
     }
